26f1181bc778e5ff64ad28e8d46279e488b4c53c
jxnblk <jacksonblack@gmail.com>
2016-04-17 18:48:02 -0400

    Replace react-tools with babel

--- geomicon.js
+++ geomicon.js
@@ -1,28 +1,32 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var React = require('react');
 var paths = require('geomicons-open/src/js/paths');
 
-var Icon = React.createClass({displayName: "Icon",
+var Icon = React.createClass({
+  displayName: 'Icon',
 
-  getDefaultProps: function() {
+
+  getDefaultProps: function getDefaultProps() {
     return {
       name: 'warning',
       width: '1em',
       height: '1em',
-      fill: 'currentColor',
-    }
+      fill: 'currentColor'
+    };
   },
 
-  render: function() {
+  render: function render() {
     var path = paths[this.props.name] || false;
-    return (
-      React.createElement("svg", React.__spread({},  this.props, {dataId: 'geomicon-'+this.props.name, viewBox: "0 0 32 32"}), 
-        React.createElement("path", {d: path})
-      )
-    )
+    return React.createElement(
+      'svg',
+      _extends({}, this.props, { dataId: 'geomicon-' + this.props.name, viewBox: '0 0 32 32' }),
+      React.createElement('path', { d: path })
+    );
   }
 
 });
 
 module.exports = Icon;
-
